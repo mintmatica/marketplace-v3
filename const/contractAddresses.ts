c7e2b1ad8687d08503e34c19e6f6c11206931136
mintmatica <mdptrading@gmail.com>
2023-10-28 17:43:27 -0600

    update

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,7 +7,6 @@ export const NETWORK = Ethereum;
 export const clientID = "39c33483859c464d122c124e3693ca2e";
 export const secretKey = "IJaUJfftZ0mMMRAf4tFSF53tJcIXhLQxm9LDU2o8n8w4J1iOVShMW7DYYN4MMKzb3C8SKwVFGy6eXOup1-SDzw";
 
-
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
 export const MARKETPLACE_ADDRESS = "0xBC1221FbF0BCb88E59bD48f2066aCa7731068509";
